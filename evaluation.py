7105da41ce4569cf081d76dd2c8a986c037dcbbb
Arek Kesiz Abnousi <99053976+Arek-KesizAbnousi@users.noreply.github.com>
2024-11-28 20:18:50 -0500

    Create evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,8 @@
+# evaluation.py
+
+from sklearn.metrics import accuracy_score
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    return accuracy
